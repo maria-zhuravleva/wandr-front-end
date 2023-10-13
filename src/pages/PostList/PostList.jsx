3370f8db4985790a0c202d6ac036621edd3ba898
kalyntn97 <kalyntn@gmail.com>
2023-10-13 19:03:30 -0400

    add a BlogCard component

--- src/pages/PostList/PostList.jsx
+++ src/pages/PostList/PostList.jsx
@@ -1,8 +1,16 @@
-const PostList = () => {
+//components
+import PostCard from "../../components/PostCard/PostCard"
+
+const PostList = (props) => {
   return ( 
-    <>
+    <main>
     <h1>Post List</h1>
-    </>
+    {props.posts.map(post =>  
+      <>
+      {post.isPublic && <PostCard key={post._id} post={post} />}
+      </>
+    )}
+    </main>
    )
 }
  
