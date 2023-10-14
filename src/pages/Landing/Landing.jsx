e1cba0f3fa19978da43ec4449b8510f06a13cd06
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-13 21:30:26 -0400

    create landing page layout

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
 // css
 import styles from './Landing.module.css'
+import bannerImage from "../../assets/img/default.jpg"
+import missionImage from "../../assets/img/default-pic2.jpg"
+import quotes from "../../assets/icons/quotes.png"
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
-    </main>
+    <>
+      <div className={styles.banner}>
+        <img src={bannerImage} alt="Banner" />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.column}>
+          <img src={missionImage} alt="waterfall" />
+        </div>
+        <div className={styles.column}>
+          <h5>Our Mission</h5>
+          <div className={styles.quotesWrapper}>
+            <img src={quotes} alt="quotes" className={styles.quotes} />
+          </div>
+          <p style={{ width: '200px', opacity: '1' }}>Explore the world, and let every adventure be a story worth telling</p>
+          <div className={styles.linkWrapper}>
+            <Link to="/about" className={styles.customLink}>Learn more</Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.topPosts}>
+          <h3>Top Posts</h3>
+        <div className={styles.topPostsContent}>
+          <img src={missionImage} alt="waterfall" />
+          <img src={missionImage} alt="waterfall" />
+          <img src={missionImage} alt="waterfall" />
+        </div>
+      </div>
+    </>
   )
 }
 
