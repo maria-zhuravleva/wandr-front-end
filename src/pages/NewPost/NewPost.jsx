21bcf404e5d2b72b63d160d19f0446d4a603d8bb
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-14 14:03:14 -0400

    add css style to new post page

--- src/pages/NewPost/NewPost.jsx
+++ src/pages/NewPost/NewPost.jsx
@@ -24,50 +24,64 @@ const NewPost = (props) => {
   }
 
   return ( 
-    <main className={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title-input">Title</label>
-        <input
-          required
-          type="text"
-          name="title"
-          id="title-input"
-          value={formData.title}
-          placeholder="Title"
-          onChange={handleChange}
-        />
-        <label htmlFor="location-input">Location</label>
-        <input
-          required
-          type="text"
-          name="location"
-          id="location-input"
-          value={formData.location}
-          placeholder="Location"
-          onChange={handleChange}
-        />
-        <label htmlFor="content-input">Content</label>
-        <textarea
-          required
-          type="text"
-          name="content"
-          id="content-input"
-          value={formData.content}
-          placeholder="Content"
-          onChange={handleChange}
-        />
-        <label htmlFor="public-input">Make Public?</label>
-        <input 
-          type="checkbox"
-          name="public"
-          id="public-input"
-          value={formData.public}
-          onChange={e => handleChecked(e, !formData.public)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </main>
-   )
+    <>
+      <main className={styles.container}>
+        <h1>NEW POST</h1>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.inputWrapper}>
+            <label htmlFor="title-input">Title</label>
+            <input
+              required
+              type="text"
+              name="title"
+              id="title-input"
+              value={formData.title}
+              placeholder="Title"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.inputWrapper}>
+            <label htmlFor="location-input">Location</label>
+            <input
+              required
+              type="text"
+              name="location"
+              id="location-input"
+              value={formData.location}
+              placeholder="Location"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.inputWrapper}>
+            <label htmlFor="content-input">Content</label>
+            <textarea
+              required
+              type="text"
+              name="content"
+              id="content-input"
+              value={formData.content}
+              placeholder="Content"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.inputWrapper}>
+            <label htmlFor="public-input">Make Public?</label>
+            <input 
+              type="checkbox"
+              name="public"
+              id="public-input"
+              checked={formData.public}
+              onChange={e => handleChecked(e, !formData.public)}
+            />
+          </div>
+          <div className={styles.buttonWrapper}>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+        <div className={styles.imageWrapper}></div>
+      </main>
+    </>
+  )
 }
- 
+
 export default NewPost
