7bce329cbfd63405609f1ae18a5e9432ac0af99a
kalyntn97 <kalyntn@gmail.com>
2023-10-13 20:25:41 -0400

    add function to fetch post details page

--- src/pages/PostDetails/PostDetails.jsx
+++ src/pages/PostDetails/PostDetails.jsx
@@ -1,8 +1,25 @@
+//npm modules
+import { useState } from 'react'
+import { useParams, useNavigate, Link } from 'react-router-dom'
+//services
+import * as postService from '../../services/postService'
 //css
 import styles from './PostDetails.module.css'
 
 const PostDetails = (props) => {
-  
+  const [post, setPost] = useState(null)
+
+  const { postId } = useParams()
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const PostData = await postService.show(postId)
+      setPost(PostData)
+    }
+   fetchPost()
+  }, [blogId])
 
   return ( 
     <h1>Post Details here</h1>
