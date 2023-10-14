38c6ab28d1367d83e922d984e8f96c6c18eb89fb
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-14 08:52:17 -0600

    fill rec card with rec information

--- src/components/RecCard/RecCard.jsx
+++ src/components/RecCard/RecCard.jsx
@@ -1,7 +1,20 @@
+// css
+import styles from './RecCard.module.css'
+
 const RecCard = (props) => {
+if (!props.recommendation.name) return <h4>Write a recommendation!</h4>
+
   return ( 
     <>
-      <h1>This is a rec card</h1>
+    <header>
+      {/* author */}
+    </header>
+      <h4>{props.recommendation.name}</h4>
+      <p>{props.recommendation.activity}</p>
+      <p>{props.recommendation.time}</p>
+      <p>{props.recommendation.rating}</p>
+      {props.author === props.user.profile._id &&  <button>Delete</button>}
+      {/* {props.author === props.user.profile._id &&  <button onClick={() => props.handleDeleteRec(props.recommendation._id)}>Delete</button>} <= ADD handleDeleteRec  once backend is written for it*/} 
     </>
   )
 }
