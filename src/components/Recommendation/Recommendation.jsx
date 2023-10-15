fad835c2087270883798f1ce7443b62ab666b823
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-15 18:33:47 -0400

    add layout for text and photo on post details page

--- src/components/Recommendation/Recommendation.jsx
+++ src/components/Recommendation/Recommendation.jsx
@@ -1,6 +1,9 @@
 // npm modules
 import { useState } from "react"
 
+// css
+import styles from './Recommendation.module.css'
+
 const Recommendation = (props) => {
 
   const [rec, setRec] = useState({
@@ -20,71 +23,71 @@ const Recommendation = (props) => {
   }
 
   return (  
-    <>
+    <div className={styles.recCard}>
       <p>This is a Recommendation</p>
-      <main>
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="name-input">Name:</label>
-        <input
-          required
-          type="text"
-          name="name"
-          id="name-input"
-          value={rec.name}
-          placeholder="Name"
-          onChange={handleChange}
-        />
-        <label htmlFor="activityName-input">Activity:</label>
-        <select
-          required
-          name="activity"
-          id="activityName-input"
-          value={rec.activity}
-          onChange={handleChange}
-        >
-          <option value="Hiking">Hiking</option>
-          <option value="Star Gazing">Star Gazing</option>
-          <option value="Cycling & Mountain Biking">Cycling & Mountain Biking</option>
-          <option value="Whitewater Rafting">Whitewater Rafting</option>
-          <option value="Camping">Camping</option>
-          <option value="Restaurant">Restaurant</option>
-          <option value="Museum">Museum</option>
-          <option value="Nightlife">Nightlife</option>
-          <option value="Shopping">Shopping</option>
-          <option value="Art and Music">Art and Music</option>
-          <option value="Family-Friendly">Family-Friendly</option>
-          <option value="Scenic Views">Scenic Views</option>
-        </select>
-        <label htmlFor="time-input">How Long:</label>
-        <select
-          required
-          name="time"
-          id="time-input"
-          value={rec.time}
-          onChange={handleChange}
-        >
-          <option value="30 min">30 min</option>
-          <option value="1 hour">1 hour</option>
-          <option value="Several Hours">Several Hours</option>
-          <option value="All Day">All Day</option>
-          <option value="Weekend Trip">Weekend Trip</option>
-        </select>
-        <label htmlFor="rating-input">Rating:</label>
-        <input
-          required
-          type="number"
-          name="rating"
-          id="rating-input"
-          value={rec.rating}
-          min="1"
-          max="5"
-          placeholder="Rating"
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </main>
-    </>
+      <div>
+        <form onSubmit={handleSubmit}>
+        <label htmlFor="name-input">Name:</label>
+          <input
+            required
+            type="text"
+            name="name"
+            id="name-input"
+            value={rec.name}
+            placeholder="Name"
+            onChange={handleChange}
+          />
+          <label htmlFor="activityName-input">Activity:</label>
+          <select
+            required
+            name="activity"
+            id="activityName-input"
+            value={rec.activity}
+            onChange={handleChange}
+          >
+            <option value="Hiking">Hiking</option>
+            <option value="Star Gazing">Star Gazing</option>
+            <option value="Cycling & Mountain Biking">Cycling & Mountain Biking</option>
+            <option value="Whitewater Rafting">Whitewater Rafting</option>
+            <option value="Camping">Camping</option>
+            <option value="Restaurant">Restaurant</option>
+            <option value="Museum">Museum</option>
+            <option value="Nightlife">Nightlife</option>
+            <option value="Shopping">Shopping</option>
+            <option value="Art and Music">Art and Music</option>
+            <option value="Family-Friendly">Family-Friendly</option>
+            <option value="Scenic Views">Scenic Views</option>
+          </select>
+          <label htmlFor="time-input">How Long:</label>
+          <select
+            required
+            name="time"
+            id="time-input"
+            value={rec.time}
+            onChange={handleChange}
+          >
+            <option value="30 min">30 min</option>
+            <option value="1 hour">1 hour</option>
+            <option value="Several Hours">Several Hours</option>
+            <option value="All Day">All Day</option>
+            <option value="Weekend Trip">Weekend Trip</option>
+          </select>
+          <label htmlFor="rating-input">Rating:</label>
+          <input
+            required
+            type="number"
+            name="rating"
+            id="rating-input"
+            value={rec.rating}
+            min="1"
+            max="5"
+            placeholder="Rating"
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
