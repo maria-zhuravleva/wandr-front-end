630534d5d6cc98cc550a6d90f08ea5a9b3f04784
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-16 12:52:09 -0600

    work on conditionally showing edit form for editing a recommendation

--- src/components/EditRec/EditRec.jsx
+++ src/components/EditRec/EditRec.jsx
@@ -10,9 +10,9 @@ const EditRec = (props) => {
     setFormData({...formData, [e.target.name]: e.target.value})
   }
 
-  const handleClearField = e => {
-    setFormData({...formData, [e.target.name]: ''})
-  }
+  // const handleClearField = e => {
+  //   setFormData({...formData, [e.target.name]: ''})
+  // }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -21,8 +21,84 @@ const EditRec = (props) => {
   }
 
   return (  
-
+    <form onSubmit={handleSubmit}>
+    <label htmlFor="name-input">Name:</label>
+      <input
+        required
+        type="text"
+        name="name"
+        id="name-input"
+        value={formData.name}
+        placeholder="Name"
+        onChange={handleChange}
+      />
+      <label htmlFor="activityName-input">Activity:</label>
+      <select
+        required
+        name="activity"
+        id="activityName-input"
+        value={formData.activity}
+        onChange={handleChange}
+      >
+        <option value="Hiking">Hiking</option>
+        <option value="Star Gazing">Star Gazing</option>
+        <option value="Cycling & Mountain Biking">Cycling & Mountain Biking</option>
+        <option value="Whitewater Rafting">Whitewater Rafting</option>
+        <option value="Camping">Camping</option>
+        <option value="Restaurant">Restaurant</option>
+        <option value="Museum">Museum</option>
+        <option value="Nightlife">Nightlife</option>
+        <option value="Shopping">Shopping</option>
+        <option value="Art and Music">Art and Music</option>
+        <option value="Family-Friendly">Family-Friendly</option>
+        <option value="Scenic Views">Scenic Views</option>
+      </select>
+      <label htmlFor="time-input">How Long:</label>
+      <select
+        required
+        name="time"
+        id="time-input"
+        value={formData.time}
+        onChange={handleChange}
+      >
+        <option value="30 min">30 min</option>
+        <option value="1 hour">1 hour</option>
+        <option value="Several Hours">Several Hours</option>
+        <option value="All Day">All Day</option>
+        <option value="Weekend Trip">Weekend Trip</option>
+      </select>
+      <label htmlFor="rating-input">Rating:</label>
+      <select
+        required
+        type="number"
+        name="rating"
+        id="rating-input"
+        value={formData.rating}
+        min="1"
+        max="5"
+        step="1"
+        onChange={handleChange}
+      >
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      </select>
+      <label htmlFor="text-input">Tell us about it:</label>
+      <textarea
+        required
+        name="text"
+        id="text-input"
+        value={formData.text}
+        placeholder="I loved this activity because ..."
+        onChange={handleChange}
+        rows={4}
+        cols={30}
+      />
+      <button type="submit" className={styles.submitButton}>Submit</button>
+    </form>
   )
 }
- 
+
 export default EditRec
--- src/components/RecCard/RecCard.jsx
+++ src/components/RecCard/RecCard.jsx
@@ -1,7 +1,23 @@
+// npm modules
+import { useState } from 'react'
+
 // css
 import styles from './RecCard.module.css'
 
+// components
+import EditRec from '../EditRec/EditRec'
+
 const RecCard = (props) => {
+  const [showForm, setShowForm] = useState(false)
+
+  const handleShowForm = () => {
+    setShowForm(true)
+  }
+
+  const handleHideForm = () => {
+    setShowForm(false)
+  }
+
   return ( 
     <>
       <h4>{props.recommendation.name}</h4>
@@ -9,9 +25,17 @@ const RecCard = (props) => {
       <p>{props.recommendation.time}</p>
       <p>{props.recommendation.rating}/5</p>
       <p>{props.recommendation.text}</p>
-      {props.author._id === props.user.profile && (
+      <div className={styles.editDeleteComment}>
+      {props.author._id === props.user.profile && <button onClick={() => handleShowForm()}>Edit</button>}
+        {props.author._id === props.user.profile && (
           <button onClick={() => props.handleDeleteRec(props.recommendation._id)}>Delete</button>
         )}
+        </div>
+        <div>
+      
+      {/* show edit comment form on button click */}
+      {showForm && <EditRec rec={props.recommendation} handleEditRec={props.handleEditRec} handleHideForm={handleHideForm}/>}
+    </div>
     </>
   )
 }
