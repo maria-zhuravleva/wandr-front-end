5807ef7f0a735e5517a9cb90d1878346bf156323
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-16 16:25:44 -0600

    set up back button to return the the users profile

--- src/pages/FollowingIndex/FollowingIndex.jsx
+++ src/pages/FollowingIndex/FollowingIndex.jsx
@@ -1,12 +1,36 @@
+// npm modules
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
+
+// components
 import FollowerList from "../../components/FollowerList/FollowerList"
 import FollowingList from "../../components/FollowingList/FollowingList"
 
+// services
+import * as profileService from "../../services/profileService";
+
 const FollowingIndex = () => {
+  const { profileId } = useParams()
+  const [profile, setProfile] = useState({})
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const profileData = await profileService.showProfile(profileId)
+      setProfile(profileData)
+    }
+    fetchProfile()
+  }, [profileId])
+
+
   return (  
     <>
       <h1>This is a following index page</h1>
       <FollowerList />
       <FollowingList />
+      <Link to={`/profiles/${profile._id}`} >
+        <button>Back to {profile.name}'s profile</button>
+      </Link>
     </>
   )
 }
