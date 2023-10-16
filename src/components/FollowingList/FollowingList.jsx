d9ebaab2d629e9bb9f73f137b99d63450b01dbe0
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-16 16:06:59 -0600

    write FollowingList component

--- src/components/FollowingList/FollowingList.jsx
+++ src/components/FollowingList/FollowingList.jsx
@@ -1,7 +1,34 @@
+// npm modules
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+// services
+import * as profileService from '../../services/profileService'
+
 const FollowingList = () => {
+  const [followingList, setFollowingList] = useState([])
+  const { profileId } = useParams()
+
+  useEffect(() => {
+    const fetchFollowing = async () => {
+      const followingData = await profileService.showFollowing(profileId)
+      setFollowingList(followingData)
+    }
+    fetchFollowing()
+  },[profileId])
+
   return ( 
     <>
       <h1>This is a Following List</h1>
+      {followingList.length === 0 ? (
+      <p>Not following anyone yet</p>
+    ) : (
+      <ul>
+        {followingList.map((follower) => {
+          <li key={follower._id}>{follower.name}</li>
+        })}
+      </ul>
+    )}
     </>
   )
 }
