78533c274fefd8d3bcaeb533cb49b034c682b73f
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-16 15:58:18 -0600

    working on FollowerList component

--- src/components/FollowerList/FollowerList.jsx
+++ src/components/FollowerList/FollowerList.jsx
@@ -1,7 +1,31 @@
+// npm modules
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+// services
+import * as profileService from '../../services/profileService'
+
+
 const FollowerList = () => {
+  const [followerList, setFollowerList] = useState([])
+  const { profileId } = useParams()
+// api call useEffect
+useEffect(() => {
+  const fetchFollowers = async () => {
+    const followerData = await profileService.showFollowing(profileId)
+    setFollowerList(followerData)
+  }
+  fetchFollowers()
+},[profileId])
+
+// handleShowFollowers
+
   return (  
+
+    // set State
     <>
     <h1>List of Followers</h1>
+    
     </>
   )
 }
