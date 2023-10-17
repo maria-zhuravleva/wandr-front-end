d35aece55eeb4607a9ae4fad0e51d75594e9db2e
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-17 15:06:14 -0400

    add css to date card

--- src/components/DateCard/DateCard.jsx
+++ src/components/DateCard/DateCard.jsx
@@ -10,8 +10,9 @@ import { faCalendarDays } from '@fortawesome/free-solid-svg-icons'
 const DateCard = ({ createdAt }) => {
   const date = new Date(createdAt).toLocaleDateString()
   const Calendar = <FontAwesomeIcon icon={faCalendarDays} />
+
   return (
-    <div className={styles.container}>
+    <div className={styles.dateContainer}>
       {Calendar}
       <h5>{date}</h5>
     </div>
