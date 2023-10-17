3bae33c12a339971351f22c9ca0027e6c4e6f29c
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-16 18:39:59 -0600

    work on ExplorePage controller

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -1,9 +1,27 @@
-const ExplorePage = () => {
-  return (  
+// npm modules
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
+// services
+import * as profileService from '../../services/profileService'
+
+// css
+import styles from './ExplorePage.module.css'
+
+
+const ExplorePage = () => {
+  // set state
+  // set useParams
+
+  // useEffect for database query
+  return (  
     <>
-    
+    <header>
     <h1>Explore Page</h1>
+    </header>
+    <main>
+      {/* map over posts to a card component */}
+    </main>
 
     </>
   )
