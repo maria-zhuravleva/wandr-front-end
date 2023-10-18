27da5502fdbe6f54e13f79d460a6fdc7d84a3153
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-18 11:40:00 -0600

    update useEffect to get more data about following

--- src/components/FollowingList/FollowingList.jsx
+++ src/components/FollowingList/FollowingList.jsx
@@ -13,11 +13,22 @@ const FollowingList = () => {
 
   useEffect(() => {
     const fetchFollowing = async () => {
-      const followingData = await profileService.showFollowing(profileId)
-      setFollowingList(followingData)
-    }
-    fetchFollowing()
-  },[profileId])
+      try {
+        const followingData = await profileService.showFollowing(profileId)
+        const followingWithProfileData = await Promise.all(
+          followingData.map(async (following) => {
+            const profileData = await profileService.showProfile(following._id)
+            return { ...following, profile: profileData }
+          })
+        )
+        setFollowingList(followingWithProfileData)
+      } catch (error) {
+        console.error(error)
+      }
+    };
+  
+    fetchFollowing();
+  }, [profileId]);
 
   return ( 
     <>
@@ -27,8 +38,8 @@ const FollowingList = () => {
     ) : (
       followingList.map((follower) => (
         <Link key={follower._id} to={`/profiles/${follower._id}`}>
-          <img src={follower.photo || avatar} alt="profile image" />
-          <p>{follower.name}</p>
+          <img src={follower.profile.photo || avatar} alt="profile image" />
+          <p>{follower.profile.name}</p>
         </Link>
       ))
     )}
