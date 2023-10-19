45a5e158bd5114c3efe69c643ba3d9c57a3a11e2
kalyntn97 <kalyntn@gmail.com>
2023-10-19 15:38:13 -0400

    add a following posts

---
+++ src/components/FollowingPosts/FollowingPosts.jsx
@@ -0,0 +1,45 @@
+//npm modules
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+//services
+import {explorePage as explorePage} from '../../services/profileService'
+//components
+import PostCard from "../PostCard/PostCard"
+//css
+import styles from './FollowingPosts.module.css'
+import arrowRight from "../../assets/icons/arrow-right.png"
+
+const FollowingPosts = (props) => {
+  const [followingPosts, setFollowingPosts] = useState([])
+
+  useEffect(() => {
+    const fetchFollowingPosts = async () => {
+      try {
+        const followingPostsData = await explorePage(props.user?.profile)
+        setFollowingPosts(followingPostsData)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchFollowingPosts()
+  }, [props.user?.profile])
+
+  return ( 
+    <>
+      {followingPosts.map((post, idx) => (
+        post.public ? (
+          <React.Fragment key={post._id}>
+          {idx < 5 && <PostCard post={post} />}
+        </React.Fragment>
+      ) : null
+      ))}
+
+      <Link to={`/profiles/${props.user?.profile}/following/posts`} className={styles.landingPageArrow}>
+          <p>Explore the Most Recent Posts of your Following</p> 
+          <img src={arrowRight} alt="arrow" />
+      </Link>
+    </>
+   )
+}
+ 
+export default FollowingPosts
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,6 @@
-// import React from 'react'
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+//npm modules
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useTheme } from '../../components/ThemeContext/ThemeContext'
 // css
 import styles from './Landing.module.css'
@@ -9,32 +8,16 @@ import styles from './Landing.module.css'
 // import missionImage from "../../assets/img/default-pic2.jpg"
 import arrowRight from "../../assets/icons/arrow-right.png"
 import quotes from "../../assets/icons/quotes.png"
+import FollowingPosts from '../../components/FollowingPosts/FollowingPosts'
 
 //components
 import SearchPost from '../../components/SearchPost/SearchPost'
 import PostCard from '../../components/PostCard/PostCard'
 
-// services
-import * as profileService from '../../services/profileService'
-
 const Landing = (props) => {
   const publicPosts = props.posts.filter(post => post.public)
   const sortedPublicPosts = publicPosts.sort((b, a) => a.likes - b.likes).reverse()
   const { theme, setTheme } = useTheme()
-
-  const [followingPosts, setFollowingPosts] = useState([])
-
-  useEffect(() => {
-    const fetchFollowingPosts = async () => {
-      try {
-        const followingPostsData = await profileService.explorePage(props.user?.profile)
-        setFollowingPosts(followingPostsData)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    fetchFollowingPosts()
-  }, [props.user?.profile])
 
   return (
     <div className={styles.landingMainContainer}>
@@ -84,9 +67,9 @@ const Landing = (props) => {
         </div>
         <div className={styles.topPostsContent}>
           {sortedPublicPosts.map((post, idx) => 
-            <>
+            <React.Fragment key={idx}>
               {idx < 5 && <PostCard key={post._id} post={post}/>}
-            </>
+            </React.Fragment>
           )}
         </div>
         <div className={styles.landingPageArrowContainer}>
@@ -98,28 +81,19 @@ const Landing = (props) => {
       </div>
 
       <div className={styles.topPosts}>
-        {props.user?.profile && <div className={styles.topPostsLines}>
-          <hr className={styles.topPostsLine} />
-          <h3>Explore Posts by Those You're Following</h3>
-          <hr className={styles.topPostsLine} />
-      </div>}
-
-      <div className={styles.topPostsContent}>
-        {props.user?.profile && Array.isArray(followingPosts) && followingPosts.map((post, idx) => (
-          post.public ? (
-          <React.Fragment key={post._id}>
-            {idx < 5 && <PostCard post={post} />}
-          </React.Fragment>
-        ) : null
-  ))}
-</div>
-      
-      {props.user?.profile && <Link to={`/profiles/${props.user?.profile}/following/posts`} className={styles.landingPageArrow}>
-          <p>Explore the Most Recent Posts of your Following</p> 
-          <img src={arrowRight} alt="arrow" />
-        </Link>
-      }
+        {props.user?.profile && 
+          <div className={styles.topPostsLines}>
+            <hr className={styles.topPostsLine} />
+            <h3>Explore Posts by Those You're Following</h3>
+            <hr className={styles.topPostsLine} />
+          </div>
+        }
+        
+        <div className={styles.topPostsContent}>
+          {props.user?.profile && <FollowingPosts user={props.user} />}
+        </div>
       </div>
+
     </div>
   )
 }
