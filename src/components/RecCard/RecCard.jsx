4b791dc1a685ff2bdd22ff43ad8bb567e41105b3
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-19 20:04:02 -0400

    minor fix to cardRec

--- src/components/RecCard/RecCard.jsx
+++ src/components/RecCard/RecCard.jsx
@@ -21,11 +21,16 @@ const RecCard = (props) => {
   return (
     <div className={styles.recCardContainer}>
       <h2>Recommendation:</h2>
-      <h4> <span>Name: </span>{props.recommendation.name}</h4>
-      <p> <span>Activity:  </span>{props.recommendation.activity}</p>
-      <p> <span>How Long:  </span>{props.recommendation.time}</p>
-      <p> <span>Rating:  </span>{props.recommendation.rating}</p>
-      <p> <span>Description:  </span>{props.recommendation.text}</p>
+      <span>Name: </span>
+      <h4>{props.recommendation.name}</h4>
+      <span>Activity:  </span>
+      <p> {props.recommendation.activity}</p>
+      <span>How Long:  </span>
+      <p> {props.recommendation.time}</p>
+      <span>Rating:  </span>
+      <p> {props.recommendation.rating}</p>
+      <span>Description:  </span>
+      <p> {props.recommendation.text}</p>
       <div className={styles.recButtons}>
         {props.author?._id === props.user?.profile && <button onClick={() => handleShowForm()}>Edit</button>}
         {props.author?._id === props.user?.profile && (
